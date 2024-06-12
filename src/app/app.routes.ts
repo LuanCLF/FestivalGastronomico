9746337204fd3670d69a9ce024c73cb33243ed69
Luan Charlys <firminocharlys@gmail.com>
2024-06-12 19:42:59 -0300

    feat: pagina de contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'Detalhes', component: DetailsComponent },
+  {path: 'Contato', component: ContactComponent}
 ];
---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<section>
+    <h1>Entre em Contato</h1>
+    <p>
+      Estamos disponíveis para qualquer sugestão ou apenas para bater um papo.
+    </p>
+    <ul>
+      <li>
+        <button>
+          <a href="/Contato"
+            >Whatsapp
+            <img src="/assets/icons/whatsapp.svg" alt="ícone do whatsapp"
+          /></a>
+        </button>
+      </li>
+      <li>
+        <button>
+          <a href="https://martins.rn.gov.br/" target="_blank"
+            >Prefeitura
+            <img
+              src="/assets/icons/house.svg"
+              alt="ícone de casa representando a prefeitura"
+          /></a>
+        </button>
+      </li>
+      <li>
+        <button>
+          <a href="https://www.instagram.com/festivaldemartins/" target="_blank"
+            >Instagram
+            <img src="/assets/icons/instagram.svg" alt="ícone do instagram"
+          /></a>
+        </button>
+      </li>
+    </ul>
+  </section>`,
+  styleUrl: './contact.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactComponent {}
